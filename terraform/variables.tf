fd40917ea10db20650917125956c27c666bfbcad
Dr. Uche Madu (DVM) <29081638+uche-madu@users.noreply.github.com>
2023-01-19 12:12:55 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,4 +16,23 @@ variable "gcp_zone_1" {
   description = "GCP Zone"
 }
 
-variable "credentials_file" {}
+variable "gcp_service_list" {
+  type        = list(string)
+  description = "The list of apis necessary for the project"
+  default     = []
+}
+
+variable "account_id" {
+  description = "The service account ID. Changing this forces a new service account to be created."
+}
+
+variable "description" {
+  description = "The display name for the service account. Can be updated without creating a new resource."
+  default     = "managed-by-terraform"
+}
+
+variable "roles" {
+  type        = list(string)
+  description = "The roles that will be granted to the service account."
+  default     = []
+}
